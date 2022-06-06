e53ab478675f625b205391ae647f9567576e7a8d
OMICRON1123 <100249444+OMICRON1123@users.noreply.github.com>
2022-06-06 17:06:03 -0500

    Add files via upload

--- src/com/Login/Administrador.java
+++ src/com/Login/Administrador.java
@@ -62,6 +62,7 @@ public class Administrador extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
+        System.out.println("I am here");
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -79,7 +80,7 @@ public class Administrador extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Administrador.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+            
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
